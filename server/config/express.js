a4e2f401bdd519266919d1febb30299943980134
MARCO_B <bizo@befund.com>
2015-11-02 17:58:25 +0100

    ok

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,10 @@
 /**
  * Express configuration
  */
-
 'use strict';
 
 var express = require('express');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -28,17 +27,16 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
+
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
+    app.use(express.static(path.join(config.root, 'www')));
+    app.set('appPath', config.root + '/www');
     app.use(morgan('dev'));
   }
-
   if ('development' === env || 'test' === env) {
 //    app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
+    app.use(express.static(path.join(config.root, 'www')));
     app.set('appPath', 'client');
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
