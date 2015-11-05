f7e0e0769ba0005d8c687b17609eb5a80f28350c
MARCO_B <bizo@befund.com>
2015-11-05 18:34:10 +0100

    ok

--- server/config/SEQ_ID.js
+++ server/config/SEQ_ID.js
@@ -1,39 +1,59 @@
 (function () {
 
-// *** within initial js file of project --> set up SEQ_ID.js
-//   -->  var SEQ_ID = require("./lib/SEQ_ID");
+// *** put this SEQ_ID.js file in "config" folder
+
+// *** within initial start-up (app.js) file of project --> set up SEQ_ID.js :
+
+//   -->  var SEQ_ID = require("./config/SEQ_ID");
 //    initialize io_counters collection
 //   -->  SEQ_ID.load_SEQ_ID(mongoose.connection, 'core');
 
-// *** within each "XXX.model.js" add this after creating schema:
+// *** within each "XXX.model.js" add these lines right after your schema :
+
+//   --> var SEQ_ID = require("../../config/SEQ_ID");
 //   --> UserSchema.plugin(SEQ_ID.plugin, {counterID: 'core', prefix: 'U0'});
 
-   var counter_name, db, mongoose;
-   mongoose = require('mongoose');
-   counter_name = null;
-   db = null;
+// *** you don't have to add "SEQ" manually as part of the Schema, as this routine adds it with a "pre" function
 
-   exports.load_SEQ_ID = function (database, id_string) {
-      counter_name = 'io_counters';
+   var mongoose = require('mongoose'),
+
+      db = null,
+      collection_name = 'io_counters',
+      IO_Counter = null;
+
+   exports.load_SEQ_ID = function (database, id_string, id_name) {
       db = database;
-      var schema = new mongoose.Schema({
+      var IO_countersSchema = new mongoose.Schema({
          _id: String,
          SEQ: String,
          SEQ_num: Number,
          name: String,
          created: {type: Date, default: Date.now},
-         updated: {type: Date}
+         updated: {type: Date, default: Date.now}
       });
-      var SEQ_ID_create = mongoose.model(counter_name, schema);
-      SEQ_ID_create.create({
+      try {
+         if (mongoose.model(collection_name)) {
+            console.log('SEQ_ID model - EXISTS')
+         }
+      } catch (e) {
+         //console.log('-> ' + e)
+         if (e.name === 'MissingSchemaError') {
+            console.log('SEQ_ID model - NEW')
+            IO_Counter = mongoose.model(collection_name, IO_countersSchema);
+         }
+      }
+      db.model(collection_name).create({
             _id: id_string,
-            SEQ_num: 1075000100
+            SEQ_num: 1075000100,
+            name: id_name
          },
          function (error, success) {
-            if (error) console.log("ID : '" + id_string + "' exists");
-            if (!error) console.log("ID OK : " + success);
+            if (error) console.log("SEQ_ID : " + id_string + " : exists");
+            if (!error) console.log("SEQ_ID OK : " + success);
          });
-      return db.model(counter_name, schema);
+      if (!mongoose.model(collection_name)) {
+         return db.model(collection_name, IO_countersSchema);
+      }
    };
 
    exports.plugin = function (schema, options) {
@@ -45,16 +65,18 @@
       model_name = options.counterID.toLowerCase();
       // get "prefix" to put in front of base_32 number:  "C0" + "A1B2C3D4"
       prefix = options.prefix;
-      Counter = db.model(counter_name);
+      Counter = db.model(collection_name);
       schema.add({
-         SEQ: {
+         Seq: {
             type: String,
             unique: true
          }
       });
       return schema.pre('save', function (next) {
+         var self,
+            _this = this;
          self = this;
-         if (!self.SEQ) {
+         if (!self.Seq) {
             return Counter.collection.findAndModify({
                   _id: model_name
                },
@@ -65,13 +87,12 @@
                   "new": true,
                   upsert: true
                }, function (err, doc) {
-                  //var count = new Object();
                   var count;
-                  count = doc.SEQ_num + 0 ;
+                  count = doc.SEQ_num;
                   if (err) {
                      return next(err);
                   }
-                  self.SEQ = prefix + count.toString(32).toUpperCase();
+                  self.Seq = prefix + count.toString(32).toUpperCase();
                   return next();
                });
          } else {
