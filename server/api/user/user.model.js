789a57eee4170054daefe0874d8cbae8905654ef
MARCO_B <bizo@befund.com>
2015-11-07 11:58:33 +0100

    ok

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -1,66 +1,106 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var crypto = require('crypto');
+var mongoose = require('mongoose'),
+// TODO: point to another MongoDB server in future apps
+//   mongooseConfig = require("../../config/mongoose"),
+   Schema = mongoose.Schema,
+   crypto = require('crypto'),
+   timestamps = require('mongoose-timestamp'),
+   mongooseTypes = require("mongoose-types"),
+   SEQ_ID = require("../../config/SEQ_ID");
+
+mongooseTypes.loadTypes(mongoose);
+
+// TODO: point to another MongoDB server in future apps
+// var connection = mongooseConfig.use('local');
+var connection = mongoose;
+
+var Email = mongoose.SchemaTypes.Email;
+var URL = mongoose.SchemaTypes.Url;
 
 var UserSchema = new Schema({
-  SEQ: String,
-  name: String,
-  username: String,
-  email: {type: String, lowercase: true},
-  role: {
-    type: String,
-    default: 'user'
-  },
-  hashedPassword: String,
-  provider: String,
-  salt: String,
-  detail: {
-    first_name: String,
-    last_name: String,
-    phone: String,
-    image_24: String,
-    image_32: String,
-    image_48: String,
-    image_72: String,
-    image_192: String
-  }
+   username: {type: String, lowercase: true},
+   firstName: {type: String},
+   lastName: {type: String},
+   email: {type: Email, lowercase: true},
+   phoneNumbers: [
+      {type: String, value: String}
+   ],
+   notifySMS: String,
+   addresses: [{
+      type: String,
+      address1: String,
+      address2: String,
+      city: String,
+      province: String,
+      postCode: String,
+      country: String
+   }],
+   companySEQ: String,
+   title: String,
+   role: {
+      type: String,
+      default: 'user'
+   },
+   background: String,
+   language: String,
+   avatar: String,
+   photos: [
+      {type: String, size: String, url: URL}
+   ],
+   lastLogin: {type: Date},
+   createdBy: {type: String},
+   hashedPassword: {type: String},
+   provider: {type: String},
+   salt: {type: String}
+});
+
+//UserSchema.plugin(genSEQ, {
+//counterID: 'SEQ',
+//prefix: 'US'
+//});
+
+UserSchema.plugin(SEQ_ID.plugin, {counterID: 'core', prefix: 'US'});
+
+UserSchema.plugin(timestamps, {
+   createdAt: 'tsCreate',
+   updatedAt: 'tsUpdate'
 });
 
 /**
  * Virtuals
  */
 UserSchema
-  .virtual('password')
-  .set(function (password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashedPassword = this.encryptPassword(password);
-  })
-  .get(function () {
-    return this._password;
-  });
+   .virtual('password')
+   .set(function (password) {
+      this._password = password;
+      this.salt = this.makeSalt();
+      this.hashedPassword = this.encryptPassword(password);
+   })
+   .get(function () {
+      return this._password;
+   });
 
 // Public profile information
 UserSchema
-  .virtual('profile')
-  .get(function () {
-    return {
-      'name': this.name,
-      'role': this.role
-    };
-  });
+   .virtual('profile')
+   .get(function () {
+      return {
+         'firstName': this.firstName,
+         'lastName': this.lastName,
+         'role': this.role
+      };
+   });
 
 // Non-sensitive info we'll be putting in the token
 UserSchema
-  .virtual('token')
-  .get(function () {
-    return {
-      '_id': this._id,
-      'role': this.role
-    };
-  });
+   .virtual('token')
+   .get(function () {
+      return {
+         '_id': this._id,
+         'role': this.role
+      };
+   });
 
 /**
  * Validations
@@ -68,94 +108,88 @@ UserSchema
 
 // Validate empty email
 UserSchema
-  .path('email')
-  .validate(function (email) {
-    return email.length;
-  }, 'Email cannot be blank');
+   .path('email')
+   .validate(function (email) {
+      return email.length;
+   }, 'Email cannot be blank');
 
 // Validate empty password
 UserSchema
-  .path('hashedPassword')
-  .validate(function (hashedPassword) {
-    return hashedPassword.length;
-  }, 'Password cannot be blank');
+   .path('hashedPassword')
+   .validate(function (hashedPassword) {
+      return hashedPassword.length;
+   }, 'Password cannot be blank');
 
 // Validate email is not taken
 UserSchema
-  .path('email')
-  .validate(function (value, respond) {
-    var self = this;
-    this.constructor.findOne({email: value}, function (err, user) {
-      if (err) throw err;
-      if (user) {
-        if (self.id === user.id) return respond(true);
-        return respond(false);
-      }
-      respond(true);
-    });
-  }, 'The specified email address is already in use.');
+   .path('email')
+   .validate(function (value, respond) {
+      var self = this;
+      this.constructor.findOne({email: value}, function (err, user) {
+         if (err) throw err;
+         if (user) {
+            if (self.id === user.id) return respond(true);
+            return respond(false);
+         }
+         respond(true);
+      });
+   }, 'The specified email address is already in use.');
 
 var validatePresenceOf = function (value) {
-  return value && value.length;
+   return value && value.length;
 };
 
 /**
  * Pre-save hook
  */
 UserSchema
-  .pre('save', function (next) {
-    if (!this.isNew) return next();
-
-    if (!validatePresenceOf(this.hashedPassword))
-      next(new Error('Invalid password'));
-    else
-      next();
-  });
-
-
-UserSchema
-  .pre('save', function (next) {
-    this.SEQ = 'U' + Math.floor((Math.random() * 100000000) + 1);
-    next();
-  });
+   .pre('save', function (next) {
+      if (!this.isNew) return next();
+      if (!validatePresenceOf(this.hashedPassword))
+         next(new Error('Invalid password'));
+      else
+         next();
+   });
 
 /**
  * Methods
  */
 UserSchema.methods = {
-  /**
-   * Authenticate - check if the passwords are the same
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
-  authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashedPassword;
-  },
+   /**
+    * Authenticate - check if the passwords are the same
+    *
+    * @param {String} plainText
+    * @return {Boolean}
+    * @api public
+    */
+   authenticate: function (plainText) {
+      return this.encryptPassword(plainText) === this.hashedPassword;
+   },
 
-  /**
-   * Make salt
-   *
-   * @return {String}
-   * @api public
-   */
-  makeSalt: function () {
-    return crypto.randomBytes(16).toString('base64');
-  },
+   /**
+    * Make salt
+    *
+    * @return {String}
+    * @api public
+    */
+   makeSalt: function () {
+      return crypto.randomBytes(16).toString('base64');
+   },
 
-  /**
-   * Encrypt password
-   *
-   * @param {String} password
-   * @return {String}
-   * @api public
-   */
-  encryptPassword: function (password) {
-    if (!password || !this.salt) return '';
-    var salt = new Buffer(this.salt, 'base64');
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
-  }
+   /**
+    * Encrypt password
+    *
+    * @param {String} password
+    * @return {String}
+    * @api public
+    */
+   encryptPassword: function (password) {
+      if (!password || !this.salt) return '';
+      var salt = new Buffer(this.salt, 'base64');
+      return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+   }
 };
 
-module.exports = mongoose.model('User', UserSchema);
+// TODO: point to another MongoDB server in future apps
+
+module.exports = connection.model('User', UserSchema);
