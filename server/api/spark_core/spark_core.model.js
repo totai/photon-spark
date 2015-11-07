3377ada0f523fe342249215cee1b5c3f8958b77a
MARCO_B <bizo@befund.com>
2015-11-07 12:03:09 +0100

    ok

--- server/api/spark_core/spark_core.model.js
+++ server/api/spark_core/spark_core.model.js
@@ -1,16 +1,17 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-   Schema = mongoose.Schema,
-   SEQ_ID = require("../../config/SEQ_ID");
+   Schema = mongoose.Schema;
 
 var Spark_coreSchema = new Schema({
+   _id: String,
+   name: String,
    ownerSEQ: String,
    coreid: String,
-   name: {type: String, unique: true},
+   location: String,
+   comment: String,
+   types: [],
    created: {type: Date, default: Date.now}
 });
-
-Spark_coreSchema.plugin(SEQ_ID.plugin, {counterID: 'core', prefix: 'C0'});
 
 module.exports = mongoose.model('Spark_core', Spark_coreSchema);
