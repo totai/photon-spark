900163936b66caceaef5cdf59b7d4e1049e96071
MARCO_B <bizo@befund.com>
2015-11-07 17:45:05 +0100

    ok

--- server/api/spark_core/spark_core.model.js
+++ server/api/spark_core/spark_core.model.js
@@ -1,17 +1,21 @@
 'use strict';
 
 var mongoose = require('mongoose'),
+   timestamps = require('mongoose-timestamp'),
    Schema = mongoose.Schema;
 
 var Spark_coreSchema = new Schema({
-   _id: String,
    name: String,
    ownerSEQ: String,
    coreid: String,
    location: String,
    comment: String,
    types: [],
-   created: {type: Date, default: Date.now}
+});
+
+UserSchema.plugin(timestamps, {
+   createdAt: 'tsCreate',
+   updatedAt: 'tsUpdate'
 });
 
 module.exports = mongoose.model('Spark_core', Spark_coreSchema);
