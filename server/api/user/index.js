8ed272e8a4cba36ae343275ef665d14901cb068d
MARCO_B <bizo@befund.com>
2015-11-09 11:47:28 +0100

    ok

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -7,7 +7,9 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/', controller.index);
+
+router.get('/TEMP', auth.isAuthenticated(), controller.index);
 router.delete('/:id', auth.isAuthenticated(), controller.destroy); // auth.hasRole('admin')
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
